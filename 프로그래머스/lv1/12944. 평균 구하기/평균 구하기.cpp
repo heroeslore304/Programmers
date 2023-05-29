ce1e3d27ba86f1e8783a989fe1e6575b2e4a127c
heroeslore304 <65457127+heroeslore304@users.noreply.github.com>
2023-05-29 17:06:46 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    double size = arr.size();
+    double sum = 0;
+    for(int i =0;i<size;i++)
+    {
+        sum +=arr[i];
+    }
+    answer = sum/size;
+    return answer;
+}
